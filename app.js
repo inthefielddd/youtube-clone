1df57fd2af7bf1d9cd62fcaa0fd84f244ad7a999
hyeon ji <inthefield1027@gmail.com>
2020-11-24 00:15:59 +0900

    add pugs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middleware";
 import userRouter  from "./routers/userRouter";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
@@ -10,12 +11,16 @@ import routes from "./routes";
 
 const app = express();
 
+app.set("view engine", "pug")
 // middleware
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended :true}));
 app.use(morgan("dev"));
-app.use(helmet());
+
+//locals
+app.use(localsMiddleware)
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
