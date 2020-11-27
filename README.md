0140a8bdb3d4e77bce92de7e94c96c974e00db3c
hyeon ji <inthefield1027@gmail.com>
2020-11-27 14:55:51 +0900

    add pages

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Youtube Clone
 
 Cloning youtube with NodeJS and VanillaJS
+
+## Pages:
+
+-[ ] Home
+-[x] Join
+-[x] Login
+-[x] Search
+-[ ] User Detail
+-[x] Edit Profile
+-[x] Change Password
+-[ ] Video Detail
+-[x] Edit Video
+-[x] Upload
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -4,10 +4,10 @@ import routes from "../routes";
 
 const videoRouter = express.Router();
 
+videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload,upload);
 videoRouter.get(routes.videoDetail,videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
