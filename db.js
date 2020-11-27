77e389c7e46955c0b7749306b19041199e336c33
hyeon ji <inthefield1027@gmail.com>
2020-11-27 15:49:01 +0900

    add mixins

---
+++ db.js
@@ -0,0 +1,62 @@
+export const videos = [
+    {
+        id:123456,
+        title: "What I Love",
+        description: "This is something i love",
+        views:43,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id:951027,
+            name : "hyeon ji",
+            email: "chl12005@naver.com"
+        }
+    },
+    {
+       id:987654,
+       title: "What I eat",
+       description: "This is food i love",
+       views:21,
+       videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+       creator: {
+           id:777777,
+           name : "ham cheese",
+           email: "inthecheese@naver.com"
+       },
+   },
+   {
+       id:348298,
+       title: "What I drink",
+       description: "This is drink i love",
+       views:33,
+       videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+       creator: {
+           id:482784,
+           name : " dan bi",
+           email: "danbbb12@naver.com"
+       },
+   },
+   {
+       id:424829,
+       title: "랄랄라랄",
+       description: "This is something i love",
+       views:3232,
+       videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+       creator: {
+           id:328133,
+           name : "eun su",
+           email: "jjj143@naver.com"
+       },
+   },
+   {
+       id:123456,
+       title: "travel",
+       description: "This is something i love",
+       views:1000,
+       videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+       creator: {
+           id:951027,
+           name : "kkkkk",
+           email: "kkk123@naver.com"
+       },
+   }
+]
