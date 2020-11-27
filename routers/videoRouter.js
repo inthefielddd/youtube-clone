17faee2e6f561e7a0ab4ee856e151b4fd8fa5a42
hyeon ji <inthefield1027@gmail.com>
2020-11-27 20:21:51 +0900

    join login upload controller

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { deleteVideo, editVideo, upload, videoDetail, videos } from "../controllers/videoController";
+import { deleteVideo, editVideo, getUpload, postUpload, videoDetail, videos } from "../controllers/videoController";
 import routes from "../routes";
 
 const videoRouter = express.Router();
 
+//upload
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+
+videoRouter.get(routes.videoDetail(),videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.upload,upload);
-videoRouter.get(routes.videoDetail,videoDetail);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
