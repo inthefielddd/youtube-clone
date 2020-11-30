3b02123077cbf91a1c74b87c88f6f24186ee767d
hyeon ji <inthefield1027@gmail.com>
2020-11-30 16:28:37 +0900

    add mongoDB and mongoose and dotenv

--- init.js
+++ init.js
@@ -1,9 +1,12 @@
 import app from "./app";
+import "./db";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 2000;
+const PORT = process.env.PORT || 2000;
 
-const handleListening = (req, res) =>{
-    console.log(`✅ Listening on : http://localhost:${PORT}`)
-}
+const handleListening = (req, res) => {
+    console.log(`✅ Listening on : http://localhost:${PORT}`);
+};
 
 app.listen(PORT, handleListening);
